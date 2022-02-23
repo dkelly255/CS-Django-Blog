c4f661a890559ed297e5f8ff690a7e0026f817b4
dkelly255 <dkelly25@hotmail.co.uk>
2022-02-23 21:23:25 +0000

    Add html templates in preparation for bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'Dan Kelly',
+        'title':'Blog Post 1',
+        'content':'First Post Content',
+        'date_posted':'August 27, 2021'
+    },
+    {
+        'author':'Jane Does',
+        'title':'Blog Post 2',
+        'content':'Second Post Content',
+        'date_posted':'August 28, 2021'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
 
