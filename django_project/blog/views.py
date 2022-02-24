1e7e3a281a27f2bf2812676b2e4170e427498fc5
dkelly255 <dkelly25@hotmail.co.uk>
2022-02-24 18:51:32 +0000

    Add new view to show post details

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
+# Function-based home view
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+# Class-based home view
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # Naming convention: <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    
+
+class PostDetailView(DetailView):
+    model = Post
+    
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
+
+
 
